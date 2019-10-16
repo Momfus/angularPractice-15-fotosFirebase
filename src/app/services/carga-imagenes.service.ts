36f07a52930778edfd56ac8fdf2e4be4c7944733
Momfus <momfus@outlook.com>
2019-10-16 16:13:36 -0300

    Terminado seccion 19

--- src/app/services/carga-imagenes.service.ts
+++ src/app/services/carga-imagenes.service.ts
@@ -17,9 +17,53 @@ export class CargaImagenesService {
 
   cargarImagenesFirebase( imagenes: FileItem[] )  {
 
-    console.log( imagenes );
+    const storageRef = firebase.storage().ref(); // Referencia para la base del Api de firebase
 
+    // Barrido de todas las imagenes subidas
+    for ( const item of imagenes  ) {
 
+      item.estaSubiendo = true;
+      if ( item.progreso >= 100 ) {
+        continue; // Si ya se subio, saltearse a la siguiente iteración (saltea lo que sigue del bloque)
+      }
+
+      const uploadTask: firebase.storage.UploadTask = storageRef.child(`${ this.CARPETA_IMAGENES }/${ item.nombreArchivo }`)
+                                                                .put(item.archivo);
+
+      // Se actualiza y disparen eventos cuando cambia el estado de lo aclarado arriba.
+      uploadTask.on( firebase.storage.TaskEvent.STATE_CHANGED,
+
+        ( snapshot: firebase.storage.UploadTaskSnapshot ) => {
+          item.progreso = ( snapshot.bytesTransferred / snapshot.totalBytes ) * 100;
+        },
+
+        ( error ) => console.log('Error al subir', error),
+
+        // Si todo resulta bien
+        () => {
+
+          console.log('Imagen cargada correctamente');
+          uploadTask.snapshot.ref.getDownloadURL().then(
+
+            (onfullfilled: any) => {
+              console.log('(promise) la url de descarga es  ' + onfullfilled);
+              item.url = onfullfilled;
+              item.estaSubiendo = false;
+
+              this.guardarImagen({
+                nombre: item.nombreArchivo,
+                url: item.url
+              });
+
+            },
+
+          (onrejected: any) => {
+            console.log('(promise) la url de descarga fue rechazada');
+          });
+
+        }
+      );
+    }
   }
 
   private guardarImagen( imagen: { nombre: string, url: string} ) {
